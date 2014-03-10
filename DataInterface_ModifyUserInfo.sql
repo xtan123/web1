35b3062227c92565d5b2b7912cb96c6fb71d18fe
xtan123 <xiaod88ad@yahoo.com>
2014-03-10 13:50:39 -0500

    add again

--- DataInterface_ModifyUserInfo.sql
+++ DataInterface_ModifyUserInfo.sql
@@ -4,6 +4,7 @@ SET ANSI_NULLS ON
 GO
 
 
+
 if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[DataInterface_ModifyUserInfo]') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
 drop procedure [dbo].[DataInterface_ModifyUserInfo]
 GO
